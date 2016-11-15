3ede5ef94744a96218e9e54e120a136a13e4d25a
BrunoPFontes <brunopedrosafontes@gmail.com>
2016-11-15 15:47:45 -0800

    2016.11.15

--- Comp1451/src/ca/bcit/comp1451/lesson6/a00975687/TestChess.java
+++ Comp1451/src/ca/bcit/comp1451/lesson6/a00975687/TestChess.java
@@ -5,7 +5,7 @@ public class TestChess {
 	public static void main(String[] args) {
 		ChessPiece cp1 = new Pawn(1);
 		ChessPiece cp3 = new Pawn(1);
-		//Pawn cp4 = new Pawn(1);
+		
 		
 		ChessPiece cp2 = new Queen(9);
 		
